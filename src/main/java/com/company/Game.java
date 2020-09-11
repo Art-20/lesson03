7dde8149e436e67dcc2b20ee4304570aa1c188a2
Art-20 <artemsheliuta@gmail.com>
2020-09-11 18:52:24 +0300

    HomeWork for lesson02 V-3

--- src/main/java/com/company/Game.java
+++ src/main/java/com/company/Game.java
@@ -26,6 +26,7 @@ public class Game {
                 String y = sc_y.nextLine();
                 if (y.contains("y"));
             } while (true);
+            
            // System.out.println("Ура! Угадал!!");
         }
 
